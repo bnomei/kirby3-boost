5deb3618fb89b958beab3a0add337ea690bbbb81
Bruno Meilick <b@bnomei.com>
2021-09-30 19:41:05 +0100

    :pencil: fixed typos

--- collections/boostidpages.php
+++ collections/boostidpages.php
@@ -1,7 +1,7 @@
 <?php
 
 return function ($site) {
-    // NOTE: this does not have a cached since memcached content is assumed
+    // NOTE: this does not have a cached since cached content is assumed
     // NOTE: this collection does not include drafts
     return $site->index()->filter(function ($page) {
         return $page->hasBoost() === true;
