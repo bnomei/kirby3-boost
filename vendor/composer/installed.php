ac3e93f8664748b1bce1637680e00096b3471598
Bruno Meilick <b@bnomei.com>
2021-12-13 13:10:55 +0000

    :green_heart: fixed composer dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,68 +1,69 @@
-<?php return array(
-    'root' => array(
-        'pretty_version' => '1.8.0',
-        'version' => '1.8.0.0',
-        'type' => 'kirby-plugin',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'bnomei/kirby3-boost',
-        'dev' => false,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.8.1',
+    'version' => '1.8.1.0',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'bnomei/autoloader-for-kirby' => array(
-            'pretty_version' => '1.4.0',
-            'version' => '1.4.0.0',
-            'type' => 'project',
-            'install_path' => __DIR__ . '/../bnomei/autoloader-for-kirby',
-            'aliases' => array(),
-            'reference' => 'cd512a25af2efcc031db42aa7794a0da2e8f401e',
-            'dev_requirement' => false,
-        ),
-        'bnomei/kirby3-boost' => array(
-            'pretty_version' => '1.8.0',
-            'version' => '1.8.0.0',
-            'type' => 'kirby-plugin',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => NULL,
-            'dev_requirement' => false,
-        ),
-        'getkirby/composer-installer' => array(
-            'pretty_version' => '1.2.1',
-            'version' => '1.2.1.0',
-            'type' => 'composer-plugin',
-            'install_path' => __DIR__ . '/../getkirby/composer-installer',
-            'aliases' => array(),
-            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
-            'dev_requirement' => false,
-        ),
-        'mustangostang/spyc' => array(
-            'pretty_version' => '0.6.3',
-            'version' => '0.6.3.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../mustangostang/spyc',
-            'aliases' => array(),
-            'reference' => '4627c838b16550b666d15aeae1e5289dd5b77da0',
-            'dev_requirement' => false,
-        ),
-        'symfony/finder' => array(
-            'pretty_version' => 'v4.4.30',
-            'version' => '4.4.30.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../symfony/finder',
-            'aliases' => array(),
-            'reference' => '70362f1e112280d75b30087c7598b837c1b468b6',
-            'dev_requirement' => false,
-        ),
-        'symfony/polyfill-php80' => array(
-            'pretty_version' => 'v1.23.1',
-            'version' => '1.23.1.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../symfony/polyfill-php80',
-            'aliases' => array(),
-            'reference' => '1100343ed1a92e3a38f9ae122fc0eb21602547be',
-            'dev_requirement' => false,
-        ),
+    'reference' => NULL,
+    'name' => 'bnomei/kirby3-boost',
+  ),
+  'versions' => 
+  array (
+    'bnomei/autoloader-for-kirby' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cd512a25af2efcc031db42aa7794a0da2e8f401e',
     ),
+    'bnomei/kirby3-boost' => 
+    array (
+      'pretty_version' => '1.8.1',
+      'version' => '1.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'getkirby/composer-installer' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+    ),
+    'mustangostang/spyc' => 
+    array (
+      'pretty_version' => '0.6.3',
+      'version' => '0.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4627c838b16550b666d15aeae1e5289dd5b77da0',
+    ),
+    'symfony/finder' => 
+    array (
+      'pretty_version' => 'v4.4.30',
+      'version' => '4.4.30.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '70362f1e112280d75b30087c7598b837c1b468b6',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.23.1',
+      'version' => '1.23.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1100343ed1a92e3a38f9ae122fc0eb21602547be',
+    ),
+  ),
 );
